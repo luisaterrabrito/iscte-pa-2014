f92e4119f260919b7bb6a7c00358d95b9a0bb81f
lcmms <lcmms@iscte-iul.pt>
2014-11-13 17:49:55 +0000

    Início

---
+++ pa.iscde.filtersearch/src/pa/iscde/filtersearch/SearchView.java
@@ -0,0 +1,22 @@
+package pa.iscde.filtersearch;
+
+import java.util.Map;
+
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Composite;
+
+import pt.iscte.pidesco.extensibility.PidescoView;
+
+public class SearchView implements PidescoView {
+
+	public SearchView() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createContents(Composite viewArea, Map<String, Image> imageMap) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
