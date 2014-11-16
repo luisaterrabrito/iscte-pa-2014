030e528c59ceaf4f70666dda5254ccd533afbcc4
rnnma <rnnma@iscte-iul.pt>
2014-11-16 03:18:11 +0000

    Encontrada uma hipótese de solução para conseguir chegar à pasta dos snippets.

--- pa.iscde.snippets/src/pa/iscde/snippets/PidescoView1.java
+++ pa.iscde.snippets/src/pa/iscde/snippets/PidescoView1.java
@@ -1,25 +1,10 @@
 package pa.iscde.snippets;
 
+import java.io.File;
 import java.util.Map;
-
-
-
-
-
-
-
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Text;
 
 import pt.iscte.pidesco.extensibility.PidescoView;
 
@@ -27,13 +12,16 @@ public class PidescoView1 implements PidescoView {
 
 	@Override
 	public void createContents(Composite viewArea, Map<String, Image> imageMap) {
-//		Label label = new Label(viewArea, SWT.NONE);
-//		label.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false,
-//				false));
-//		label.setText("yah va fica");
 		
 		new SnippetsExplorer(viewArea, SWT.NONE);
 
+		File test = new File(PidescoView1.class.getProtectionDomain().getCodeSource().getLocation().getPath().replace("/", "\\") + "\\Snippets");
+		System.out.println(test.getAbsolutePath());
+		System.out.println(test.exists());
+		File[] files = test.listFiles();
+		for (int i = 0; i < files.length; i++) {
+			System.out.println(files[i].getAbsolutePath());
+		}
 	}
 
 }
