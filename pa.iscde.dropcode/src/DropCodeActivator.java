4c97b50312085cf9f6e19d7b5cf3dbb41a603746
mgpft <mgpft@iscte-iul.pt>
2014-11-18 16:24:36 +0000

    DropCodeActivator created

---
+++ pa.iscde.dropcode/src/DropCodeActivator.java
@@ -0,0 +1,17 @@
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class DropCodeActivator implements BundleActivator {
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
