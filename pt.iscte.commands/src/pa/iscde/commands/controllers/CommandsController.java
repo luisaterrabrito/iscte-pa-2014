df20eaa4e63d0bc0a149e57ab9d7f04081f0664d
farms <farms@iscte-iul.pt>
2014-11-19 22:53:43 +0000

    Adicionado o activador do componente

---
+++ pt.iscte.commands/src/pa/iscde/commands/controllers/CommandsController.java
@@ -0,0 +1,20 @@
+package pa.iscde.commands.controllers;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class CommandsController implements BundleActivator {
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
