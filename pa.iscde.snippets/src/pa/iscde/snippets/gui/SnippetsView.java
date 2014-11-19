3758413b47e4e489f32decefb5de6a2e3946d65b
rnnma@iscte-iul.pt <rnnma@iscte-iul.pt@1ba3a7cf-5a9b-5090-5aa7-a9fa95ec31b7>
2014-11-19 19:50:08 +0000

    - ID da extensão da view volta ao original. - Criada forma melhor de aceder à pasta snippet no código

--- pa.iscde.snippets/src/pa/iscde/snippets/gui/SnippetsView.java
+++ pa.iscde.snippets/src/pa/iscde/snippets/gui/SnippetsView.java
@@ -1,52 +1,62 @@
 package pa.iscde.snippets.gui;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.Map;
 
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
+import org.osgi.framework.Bundle;
 
 import pt.iscte.pidesco.extensibility.PidescoView;
 
 public class SnippetsView implements PidescoView {
 	private Composite viewArea;
 	private static SnippetsView instance;
-	
-	public static SnippetsView getInstance(){
+	private File snippetsRootFolder;
+
+	public static SnippetsView getInstance() {
 		return instance;
 	}
-	
+
 	@Override
 	public void createContents(Composite viewArea, Map<String, Image> imageMap) {
 		instance = this;
+
 		this.viewArea = viewArea;
 		
-		createExplorer();
-
-		
-		File test = new File(SnippetsView.class.getProtectionDomain().getCodeSource().getLocation().getPath().replace("/", "\\") + "\\Snippets");
-		System.out.println(test.getAbsolutePath());
-		System.out.println(test.exists());
-		File[] files = test.listFiles();
-		for (int i = 0; i < files.length; i++) {
-			System.out.println(files[i].getAbsolutePath());
+		URL fileURL;
+		try {
+			fileURL = new URL("platform:/plugin/pa.iscde.snippets/Snippets");
+			System.out.println(FileLocator.resolve(fileURL).toString());
+			snippetsRootFolder = new File(FileLocator.resolve(fileURL).getPath());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+
+		createExplorer();
 	}
-	
-	public void createExplorer(){
+
+	public void createExplorer() {
 		new SnippetsExplorer(viewArea, SWT.NONE);
 		viewArea.layout();
 	}
-	
+
 	// With file
-	public void createSnippetCode(File snp){
+	public void createSnippetCode(File snp) {
 		new SnippetCode(snp, viewArea, SWT.NONE);
 		viewArea.layout();
 	}
 
-	//Without file
-	public void createSnippetCode(){
+	// Without file
+	public void createSnippetCode() {
 		new SnippetCode(viewArea, SWT.NONE);
 		viewArea.layout();
 	}
