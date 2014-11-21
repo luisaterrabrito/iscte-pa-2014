e45a824356de26a697cf1ab41abac91c483ad419
rnnma@iscte-iul.pt <rnnma@iscte-iul.pt@1ba3a7cf-5a9b-5090-5aa7-a9fa95ec31b7>
2014-11-21 14:27:01 +0000

    - Added procedure to load snippets to the snippets explorer - Added procedure to filter snippets by language - Added procedure to filter snippets by a given text - Added procedure to call the snippet code composite with the selected snippet - Modified fonts and some sizes in the snippet explorer composite

--- pa.iscde.snippets/src/pa/iscde/snippets/gui/SnippetsExplorer.java
+++ pa.iscde.snippets/src/pa/iscde/snippets/gui/SnippetsExplorer.java
@@ -6,6 +6,14 @@ import java.util.HashMap;
 import java.util.Set;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.KeyAdapter;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.KeyListener;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -16,9 +24,10 @@ import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.List;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.wb.swt.SWTResourceManager;
 
 public class SnippetsExplorer extends Composite {
-	private Text text;
+	private Text searchText;
 	private HashMap<File, ArrayList<File>> loadedSnippets;
 	private HashMap<String, File> filteredSnippets;
 	private List snippetsList;
@@ -42,25 +51,76 @@ public class SnippetsExplorer extends Composite {
 		searchComposite.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true,
 				false, 1, 1));
 
-		text = new Text(searchComposite, SWT.BORDER);
-		text.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		searchText = new Text(searchComposite, SWT.BORDER);
+		searchText.setFont(SWTResourceManager.getFont("Segoe UI", 11,
+				SWT.NORMAL));
+		searchText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true,
+				false, 1, 1));
+		searchText.addModifyListener(new ModifyListener() {
+
+			@Override
+			public void modifyText(ModifyEvent e) {
+				if (searchText.getText().equals(""))
+					filterByLanguage();
+				else
+					search();
+			}
+		});
+		searchText.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.keyCode == SWT.CR)
+					search();
+			}
+		});
 
 		Button btnNewButton = new Button(searchComposite, SWT.NONE);
-		btnNewButton.setText("New Button");
+		btnNewButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				search();
+			}
+		});
+		btnNewButton.setFont(SWTResourceManager.getFont("Segoe UI", 11,
+				SWT.NORMAL));
+		btnNewButton.setText("Search");
 
 		Composite chooseComposite = new Composite(this, SWT.NONE);
 		chooseComposite.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true,
 				false, 1, 1));
 		chooseComposite.setLayout(new FillLayout(SWT.HORIZONTAL));
 
-		languagesCombo = new Combo(chooseComposite, SWT.NONE);
+		languagesCombo = new Combo(chooseComposite, SWT.NONE | SWT.READ_ONLY);
+		languagesCombo.setFont(SWTResourceManager.getFont("Segoe UI", 11,
+				SWT.NORMAL));
+
+		languagesCombo.addSelectionListener(new SelectionAdapter() {
+
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				filterByLanguage();
+			}
+		});
+		;
 
 		Composite snippetsComposite = new Composite(this, SWT.NONE);
 		snippetsComposite.setLayout(new FillLayout(SWT.HORIZONTAL));
-		snippetsComposite.setLayoutData(new GridData(SWT.FILL, SWT.CENTER,
-				true, true, 1, 1));
+		snippetsComposite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true,
+				true, 1, 1));
 
-		snippetsList = new List(snippetsComposite, SWT.BORDER | SWT.V_SCROLL);
+		snippetsList = new List(snippetsComposite, SWT.BORDER | SWT.V_SCROLL
+				| SWT.READ_ONLY);
+		snippetsList.setFont(SWTResourceManager.getFont("Segoe UI", 12,
+				SWT.NORMAL));
+		snippetsList.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				dispose();
+				SnippetsView.getInstance().createSnippetCode(
+						filteredSnippets.get(snippetsList.getItem(snippetsList
+								.getSelectionIndex())));
+			}
+		});
 
 		Composite addComposite = new Composite(this, SWT.NONE);
 		addComposite.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true,
@@ -68,22 +128,17 @@ public class SnippetsExplorer extends Composite {
 		addComposite.setLayout(new FillLayout(SWT.HORIZONTAL));
 
 		Button addNewButton = new Button(addComposite, SWT.NONE);
-
-		// My listener - Diogo
-		Listener listener = new Listener() {
-			public void handleEvent(Event e) {
-				switch (e.type) {
-				case SWT.Selection:
-					dispose();
-					SnippetsView.getInstance().createSnippetCode();
-					parent.layout();
-					System.out.println("Button pressed");
-					break;
-				}
+		addNewButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				dispose();
+				SnippetsView.getInstance().createSnippetCode();
 			}
-		};
+		});
+		addNewButton.setFont(SWTResourceManager.getFont("Segoe UI", 10,
+				SWT.NORMAL));
+
 		addNewButton.setText("Add New Snippet");
-		addNewButton.addListener(SWT.Selection, listener);
 
 		loadSnippets();
 	}
@@ -94,6 +149,7 @@ public class SnippetsExplorer extends Composite {
 
 		languagesCombo.removeAll();
 		languagesCombo.add("All");
+		languagesCombo.select(0);
 
 		File root = SnippetsView.getSnippetsRootFolder();
 		if (root.isDirectory()) {
@@ -109,21 +165,59 @@ public class SnippetsExplorer extends Composite {
 					}
 				}
 			}
-			for (File snippetsOfLanguage : loadedSnippets.keySet()) {
-				ArrayList<File> snippets = loadedSnippets
-						.get(snippetsOfLanguage);
-				for (File file : snippets) {
-					filteredSnippets.put(file.getName(), file);
-				}
-			}
+			loadAllToFiltered();
 		}
 
 		refreshSnippetsList();
+	}
+
+	private void loadAllToFiltered() {
+		for (File snippetsOfLanguage : loadedSnippets.keySet()) {
+			ArrayList<File> snippets = loadedSnippets.get(snippetsOfLanguage);
+			for (File file : snippets) {
+				filteredSnippets.put(file.getName().replace(".snp", ""), file);
+			}
+		}
 	}
 
 	private void refreshSnippetsList() {
 		snippetsList.removeAll();
 		snippetsList.setItems(filteredSnippets.keySet().toArray(
 				new String[filteredSnippets.keySet().size()]));
+	}
+
+	private void filterByLanguage() {
+		int selected = languagesCombo.getSelectionIndex();
+		if (selected == 0) {
+			loadAllToFiltered();
+			refreshSnippetsList();
+		} else {
+			String name = languagesCombo.getItem(selected);
+			ArrayList<File> files = null;
+			for (File snippetsOfLanguage : loadedSnippets.keySet()) {
+				if (snippetsOfLanguage.getName().equals(name)) {
+					files = loadedSnippets.get(snippetsOfLanguage);
+					break;
+				}
+			}
+			filteredSnippets.clear();
+			for (File file : files) {
+				filteredSnippets.put(file.getName().replace(".snp", ""), file);
+			}
+			refreshSnippetsList();
+		}
+	}
+
+	private void search() {
+		if (!searchText.getText().equals("")) {
+			HashMap<String, File> found = new HashMap<>();
+			for (String name : filteredSnippets.keySet()) {
+				if (name.toLowerCase().contains(
+						searchText.getText().toLowerCase()))
+					found.put(name, filteredSnippets.get(name));
+			}
+			filteredSnippets = found;
+			refreshSnippetsList();
+		}
 	}
 }
