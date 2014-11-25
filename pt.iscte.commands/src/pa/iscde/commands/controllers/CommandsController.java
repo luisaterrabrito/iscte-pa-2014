0f570fe80ba3a465a00874d632ef786e7a357db3
trvms <trvms@iscte-iul.pt>
2014-11-25 19:07:21 +0000

    [TVM] - Configurações gerais do componente e a respectiva view exemplo

--- pt.iscte.commands/src/pa/iscde/commands/controllers/CommandsController.java
+++ pt.iscte.commands/src/pa/iscde/commands/controllers/CommandsController.java
@@ -1,5 +1,11 @@
 package pa.iscde.commands.controllers;
 
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
@@ -7,13 +13,37 @@ public class CommandsController implements BundleActivator {
 
 	@Override
 	public void start(BundleContext context) throws Exception {
-		// TODO Auto-generated method stub
-
+		
+		
+		System.out.println("cenass");
+		new Runnable() {
+			public void run() {
+				
+				
+				System.out.println("cenass 2");
+				new KeyAdapter() {
+					
+					@Override
+					public void keyPressed(KeyEvent e) {
+						System.out.println("cenass 3");
+						super.keyPressed(e);
+						System.out.println("Estás a bombar: " + e.getKeyChar());
+						
+						
+						
+					}
+				
+				};
+				
+				
+			}
+		}.run();
+		
 	}
 
 	@Override
 	public void stop(BundleContext context) throws Exception {
-		// TODO Auto-generated method stub
+		
 
 	}
 
