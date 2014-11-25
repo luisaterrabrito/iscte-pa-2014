262c45dfdde71eb5a7ba783a8bc9e0b47cd388bd
lcmms <lcmms@iscte-iul.pt>
2014-11-25 18:55:56 +0000

    Interface implementada para o ProjectBrowser.
    Ainda tem algum hardcode

---
+++ pa.iscde.filtersearch/src/pa/iscde/filtersearch/view/SearchProvider.java
@@ -0,0 +1,11 @@
+package pa.iscde.filtersearch.view;
+
+import java.util.List;
+
+import org.eclipse.swt.graphics.Image;
+
+public interface SearchProvider {
+
+	List<Object> getResults(String text);
+	//void setImage(Image image);
+}
