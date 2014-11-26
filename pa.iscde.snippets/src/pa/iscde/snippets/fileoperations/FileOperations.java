c87125ef81904227107c35ac59d8e3adf608ef51
dbipo <dbipo@iscte-iul.pt>
2014-11-26 17:26:09 +0000

    - Created Dialog Box package and  Language Dialog - Changed SnippetCode class - Change FileOperations

--- pa.iscde.snippets/src/pa/iscde/snippets/fileoperations/FileOperations.java
+++ pa.iscde.snippets/src/pa/iscde/snippets/fileoperations/FileOperations.java
@@ -4,16 +4,38 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+
+import org.eclipse.core.runtime.FileLocator;
 
 import pa.iscde.snippets.gui.SnippetsView;
 
 public class FileOperations {
-	
+
 	private File fileToUse;
-	
+	private File snippetsRootFolder;
+
 	public FileOperations(File f) {
 		fileToUse = f;
+		snippetsRootFolder = f.getAbsoluteFile();
+	}
+
+	public FileOperations() {
+		URL fileURL;
+		try {
+			fileURL = new URL("platform:/plugin/pa.iscde.snippets/Snippets");
+			try {
+				snippetsRootFolder = new File(FileLocator.resolve(fileURL)
+						.getPath());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	public String getFileName() {
@@ -37,13 +59,17 @@ public class FileOperations {
 		;
 		return lines;
 	}
-	
-	public String getFileType(){
-		return fileToUse.getParent().replace(SnippetsView.getInstance().getSnippetsRootFolder().getAbsolutePath() + "\\", "");
+
+	public String getFileType() {
+		return fileToUse.getParent().replace(
+				SnippetsView.getInstance().getSnippetsRootFolder()
+						.getAbsolutePath()
+						+ "\\", "");
 	}
-	
-	public void save(String name, String code, String language){
-		//TODO: Check if file exists, if not create in the appropriate folder
-		//TODO: Save to file used Name and Code from snippetNameTextBox and snippetCodeText
+
+	public void save(String name, String code, String language) {
+		// TODO: Check if file exists, if not create in the appropriate folder
+		// TODO: Save to file used Name and Code from snippetNameTextBox and
+		// snippetCodeText
 	}
 }
