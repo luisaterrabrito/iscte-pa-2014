66e7ab81995b907ca9fd1a679e3c8b0270b42646
pdcsa@iscte-iul.pt <pdcsa@iscte-iul.pt@1ba3a7cf-5a9b-5090-5aa7-a9fa95ec31b7>
2014-11-30 16:25:10 +0000

    Mockup para o Projecto feito

--- pa.iscde.outline/src/pa/iscde/outline/view/AMinhaView.java
+++ pa.iscde.outline/src/pa/iscde/outline/view/AMinhaView.java
@@ -27,9 +27,9 @@ import pt.iscte.pidesco.projectbrowser.service.ProjectBrowserServices;
 
 public class AMinhaView implements PidescoView{
 
-	private JavaEditorServices javaServices;
+//	private JavaEditorServices javaServices;
 	
-	private ProjectBrowserServices browserServices;
+//	private ProjectBrowserServices browserServices;
 	
 	
 	@Override
@@ -37,42 +37,68 @@ public class AMinhaView implements PidescoView{
 		
 		Display d = viewArea.getDisplay();
 		
-		Bundle bundle = FrameworkUtil.getBundle(AMinhaView.class);
-		BundleContext context  = bundle.getBundleContext();
-		ServiceReference<JavaEditorServices> ref = context.getServiceReference(JavaEditorServices.class);
-		javaServices = context.getService(ref);
-		
-		ServiceReference<ProjectBrowserServices> ref2 = context.getServiceReference(ProjectBrowserServices.class);
-		browserServices = context.getService(ref2);
-		
-		Tree t = new Tree(viewArea, SWT.SINGLE | SWT.BORDER);
-		TreeItem child1 = new TreeItem(t, SWT.NONE, 0);
-	    child1.setText("Item 1");
-
-	    File f = javaServices.getOpenedFile();
-	    Class c = AMinhaView.class;
-	    
-	    TreeItem child2 = new TreeItem(t, SWT.NONE, 1);
-	    child2.setText("Item 2");
-	    Image i = imageMap.get("package-x-generic.png");
-	    Image img_escalada = new Image(d, 50, 50);
+//		Bundle bundle = FrameworkUtil.getBundle(AMinhaView.class);
+//		BundleContext context  = bundle.getBundleContext();
+//		ServiceReference<JavaEditorServices> ref = context.getServiceReference(JavaEditorServices.class);
+//		javaServices = context.getService(ref);
+//		
+//		ServiceReference<ProjectBrowserServices> ref2 = context.getServiceReference(ProjectBrowserServices.class);
+//		browserServices = context.getService(ref2);
+		Image i = imageMap.get("package-x-generic.png");
+	    Image img_escalada = new Image(d, 15, 15);
 	    GC gc = new GC(img_escalada);
 	    gc.setAntialias(SWT.ON);
 	    gc.setInterpolation(SWT.HIGH);
-	    gc.drawImage(i, 0, 0, i.getBounds().width, i.getBounds().height, 0, 0, 50, 50);
+	    gc.drawImage(i, 0, 0, i.getBounds().width, i.getBounds().height, 0, 0, 15, 15);
 	    gc.dispose();
 	    i.dispose();
-	    child2.setImage(img_escalada);
-	   
+		
+		Tree t = new Tree(viewArea, SWT.SINGLE | SWT.BORDER);
+		
+//		TreeItem child1 = new TreeItem(t, SWT.NONE, 0);
+//	    child1.setText("outline");
+//	    child1.setImage(img_escalada);
+
+//	    File f = javaServices.getOpenedFile();
+	   // Class c = AMinhaView.class;
 	    
-	    TreeItem child2a = new TreeItem(child2, SWT.NONE, 0);
-	    child2a.setText("Item 2A");
+//	    TreeItem child2 = new TreeItem(child1, SWT.NONE, 0);
+//	    child2.setText("test");
+//	    child2.setImage(img_escalada);
 	    
-	    TreeItem child2b = new TreeItem(child2, SWT.NONE, 1);
-	    child2b.setText("Item 2B");
+	    TreeItem child2a = new TreeItem(t, SWT.NONE, 0);
+	    child2a.setText("TestCase");
+	    child2a.setImage(imageMap.get("class_obj.png"));
 	    
-	    TreeItem child3 = new TreeItem(t, SWT.NONE, 2);
-	    child3.setText("Item 3");
+	    TreeItem child2aI = new TreeItem(child2a, SWT.NONE, 0);
+	    child2aI.setText("a: int");
+	    child2aI.setImage(imageMap.get("field_public_obj.png"));
+	    
+	    TreeItem child2aII = new TreeItem(child2a, SWT.NONE, 1);
+	    child2aII.setText("func() : AguiasAlpiarca");
+	    child2aII.setImage(imageMap.get("409.png"));
+	    
+	    TreeItem child2aIII = new TreeItem(child2a, SWT.NONE, 2);
+	    child2aIII.setText("func2() : void");
+	    child2aIII.setImage(imageMap.get("public_co.gif"));
+	    
+	    TreeItem child2aIV = new TreeItem(child2a, SWT.NONE, 3);
+	    child2aIV.setText("doStuff() : void");
+	    child2aIV.setImage(imageMap.get("method_private_obj.png"));
+
+	    
+//	    TreeItem child3 = new TreeItem(child1, SWT.NONE, 1);
+//	    child3.setText("view");
+//	    child3.setImage(img_escalada);
+//	    
+//	    TreeItem child3a = new TreeItem(child3, SWT.NONE, 0);
+//	    child3a.setText("AMinhaView.java");
+//	    child3a.setImage(imageMap.get("class_obj.png"));
+//	    
+//	    TreeItem child3aI = new TreeItem(child3a, SWT.NONE, 0);
+//	    child3aI.setText("Ninguém vai ver isto na print");
+//	    //imagem para variável
+
 	    
 	    t.addTreeListener(new TreeListener() {
 	        public void treeExpanded(TreeEvent e) {
@@ -85,41 +111,41 @@ public class AMinhaView implements PidescoView{
 	        }
 	      });
 	    
-	    List l = new List(viewArea, SWT.BORDER | SWT.SINGLE);
-	    
-	    t.addSelectionListener(new SelectionAdapter() {
-	        public void widgetSelected(SelectionEvent e) {
-	          TreeItem ti = (TreeItem) e.item;
-	          populateList(ti.getText());
-	        }
-
-	        private void populateList(String type) {
-	          if (type.equals("Item 1")) {
-	            l.removeAll();
-	            l.add("File 1");
-	            l.add("File 2");
-	          }
-	          if (type.equals("Item 2")) {
-	            l.removeAll();
-	          }
-	          if (type.equals("Item 2A")) {
-	            l.removeAll();
-	            l.add("File 3");
-	            l.add("File 4");
-	          }
-	          if (type.equals("Item 2B")) {
-	            l.removeAll();
-	            l.add("File 5");
-	            l.add("File 6");
-	          }
-	          if (type.equals("Item 3")) {
-	            l.removeAll();
-	            l.add("File 7");
-	            l.add("File 8");
-	          }
-
-	        }
-	      });
+//	    List l = new List(viewArea, SWT.BORDER | SWT.SINGLE);
+//	    
+//	    t.addSelectionListener(new SelectionAdapter() {
+//	        public void widgetSelected(SelectionEvent e) {
+//	          TreeItem ti = (TreeItem) e.item;
+//	          populateList(ti.getText());
+//	        }
+//
+//	        private void populateList(String type) {
+//	          if (type.equals("Item 1")) {
+//	            l.removeAll();
+//	            l.add("File 1");
+//	            l.add("File 2");
+//	          }
+//	          if (type.equals("Item 2")) {
+//	            l.removeAll();
+//	          }
+//	          if (type.equals("Item 2A")) {
+//	            l.removeAll();
+//	            l.add("File 3");
+//	            l.add("File 4");
+//	          }
+//	          if (type.equals("Item 2B")) {
+//	            l.removeAll();
+//	            l.add("File 5");
+//	            l.add("File 6");
+//	          }
+//	          if (type.equals("Item 3")) {
+//	            l.removeAll();
+//	            l.add("File 7");
+//	            l.add("File 8");
+//	          }
+//
+//	        }
+//	      });
 	}
 
 }
