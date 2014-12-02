d7cb3b3015e2fdd34edb8f3496f10041540ec7d7
nbcot <nbcot@iscte-iul.pt>
2014-12-02 17:30:45 +0000

    updated

--- pa.iscde.umldiagram/src/pa/iscde/umldiagram/utils/UmlVisitor.java
+++ pa.iscde.umldiagram/src/pa/iscde/umldiagram/utils/UmlVisitor.java
@@ -2,30 +2,44 @@ package pa.iscde.umldiagram.utils;
 import java.util.ArrayList;
 
 import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.AnnotationTypeDeclaration;
 import org.eclipse.jdt.core.dom.ClassInstanceCreation;
 import org.eclipse.jdt.core.dom.ConstructorInvocation;
+import org.eclipse.jdt.core.dom.CreationReference;
 import org.eclipse.jdt.core.dom.EnumDeclaration;
 import org.eclipse.jdt.core.dom.FieldDeclaration;
 import org.eclipse.jdt.core.dom.Initializer;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.SuperConstructorInvocation;
 public class UmlVisitor extends ASTVisitor{
 
 	private ArrayList<MethodDeclaration> methods = new ArrayList<MethodDeclaration>();
 	private ArrayList<FieldDeclaration> fields = new ArrayList<FieldDeclaration>();
 	private ArrayList<EnumDeclaration> enums = new ArrayList<EnumDeclaration>();
+	private ArrayList<String> classInstances = new ArrayList<String>();
 	
 	@Override
 	public boolean visit(MethodDeclaration m) {
 		methods.add(m);
-		return false;
+		return true;
 	}
+
+
+
+
+	@Override
+	public boolean visit(ClassInstanceCreation c) {
+		classInstances.add(c.getType().toString());
+		return true;
+	}
+
 
 
 
 	@Override
 	public boolean visit(EnumDeclaration e) {
 		enums.add(e);
-		return false;
+		return true;
 	}
 
 
@@ -33,14 +47,12 @@ public class UmlVisitor extends ASTVisitor{
 	@Override
 	public boolean visit(FieldDeclaration f) {
 		fields.add(f);
-		return false;
+		return true;
 	}
 
 	public ArrayList<EnumDeclaration> getEnums() {
 		return enums;
 	}
-
-
 
 	public ArrayList<MethodDeclaration> getMethods(){
 		return methods;
@@ -48,5 +60,8 @@ public class UmlVisitor extends ASTVisitor{
 	
 	public ArrayList<FieldDeclaration> getFields(){
 		return fields;
+	}
+	public ArrayList<String> getClassInstances() {
+		return classInstances;
 	}
 }
