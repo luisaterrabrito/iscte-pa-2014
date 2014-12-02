e451d3f8abd161b043985f151de5309aa888af56
dbipo <dbipo@iscte-iul.pt>
2014-12-02 13:52:31 +0000

    - Fixed "Focus Gained" bug - Added dialog to inform that file was saved - Made "checkIfFilenameExists" recursive - Changed "newSnippetNumber" function to simpler solution - Various bug fixes

--- pa.iscde.snippets/src/pa/iscde/snippets/fileoperations/FileOperations.java
+++ pa.iscde.snippets/src/pa/iscde/snippets/fileoperations/FileOperations.java
@@ -14,6 +14,9 @@ import java.util.ArrayList;
 
 import org.eclipse.core.runtime.FileLocator;
 
+/*
+ * Class that contains various functions to work with files
+ */
 public class FileOperations {
 
 	private File fileToUse = null;
@@ -101,40 +104,68 @@ public class FileOperations {
 			System.err.println("Failed to Write To New File");
 			e.printStackTrace();
 		}
+		fileToUse = fileToSave;
 	}
 
+	// Function that checks if filename Already exists starting from root
+	// directory
+	// Calls "checkIfNameAlreadyExistsDirectory" if file in root is a directory
+	// and will throw true if it finds a file with the same name in directory or
+	// subdirectories
+	// Calls "checkIfNameAlreadyExistsFile" if file in root is a directory and
+	// will throw true if file has the same name
+	// Returns false if it doesn't find anything in root or sub-directories
 	public boolean checkIfNameAlreadyExists(String text) {
-		if (fileToUse != null) {
-			for (File childDirectories : snippetsRootFolder.listFiles()) {
-				for (File childFiles : childDirectories.listFiles()) {
-					if (childFiles.getName().equals(text + ".snp")
-							&& !childFiles.getName()
-									.equals(fileToUse.getName()))
-						return true;
-				}
-			}
-		} else {
-			for (File childDirectories : snippetsRootFolder.listFiles()) {
-				for (File childFiles : childDirectories.listFiles()) {
-					if (childFiles.getName().equals(text + ".snp"))
-						return true;
+		for (File childDirectory : snippetsRootFolder.listFiles()) {
+			if (childDirectory.isDirectory()) {
+				if (checkIfNameAlreadyExistsDirectory(text, childDirectory))
+					return true;
+			} else if (childDirectory.isFile()) {
+				if (checkIfNameAlreadyExistsFile(text, childDirectory)) {
+					return true;
 				}
 			}
 		}
 		return false;
 	}
 
-	public int numberOfNewSnippets() {
-		int aux = 0;
-		for (File childDirectories : snippetsRootFolder.listFiles()) {
-			for (File childFiles : childDirectories.listFiles()) {
-				System.out.println(childFiles.getName());
-				if (childFiles.getName().matches("New Snippet\\(\\d+\\).snp")) {
-					aux += 1;
-				}
+	// Recursive function to check folders, if it finds a file it will call
+	// "checkIfNameAlreadyExistsDirectory" to check if Name exists and will
+	// return true if it does
+	// If it doesn't find a file or a file with the same name it will return
+	// false
+	private boolean checkIfNameAlreadyExistsDirectory(String text,
+			File childDirectories) {
+		for (File childFile : childDirectories.listFiles()) {
+			if (childFile.isDirectory()) {
+				checkIfNameAlreadyExistsDirectory(text, childFile);
+			} else if (childFile.isFile()) {
+				if (checkIfNameAlreadyExistsFile(text, childFile))
+					return true;
 			}
 		}
-		System.out.println(aux);
+		return false;
+	}
+
+	// Function to check if File has the same name has the text + .snp
+	// returns false if it is the same file
+	// returns true if it does
+	// false if not
+	private boolean checkIfNameAlreadyExistsFile(String text, File f) {
+		if (fileToUse != null && f.getName().equals(fileToUse.getName()))
+			return false;
+		else if (f.getName().equals(text + ".snp"))
+			return true;
+		return false;
+	}
+	
+	//Returns the number of the Snippet that should be assigned to a new snippet
+	//Checks the lowest snippet number available
+	public int numberOfNewSnippet() {
+		int aux = 1;
+		while(checkIfNameAlreadyExists("New Snippet (" + aux + ").snp")) {
+					aux += 1;
+		}
 		return aux;
 	}
 }
