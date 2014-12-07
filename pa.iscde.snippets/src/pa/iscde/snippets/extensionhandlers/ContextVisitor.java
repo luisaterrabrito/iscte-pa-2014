a3323d7bf4b339d849b050a5fa388bf83a05221a
rnnma@iscte-iul.pt <rnnma@iscte-iul.pt@1ba3a7cf-5a9b-5090-5aa7-a9fa95ec31b7>
2014-12-07 03:15:55 +0000

    - Added more context procedures to the visitor - Updated the cursor context to match capabilities of the visitor - Updated funcionality of context manager to match changes - Modified the context extention point to allow a sequence of contexts in one point - Added test extensions to our extension points

--- pa.iscde.snippets/src/pa/iscde/snippets/extensionhandlers/ContextVisitor.java
+++ pa.iscde.snippets/src/pa/iscde/snippets/extensionhandlers/ContextVisitor.java
@@ -1,41 +1,135 @@
 package pa.iscde.snippets.extensionhandlers;
 
+import java.lang.reflect.Modifier;
+
 import org.eclipse.jdt.core.dom.ASTVisitor;
-import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.CatchClause;
+import org.eclipse.jdt.core.dom.ForStatement;
+import org.eclipse.jdt.core.dom.IfStatement;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.TryStatement;
+import org.eclipse.jdt.core.dom.TypeDeclaration;
+
+import pa.iscde.snippets.external.CursorContext;
 
 public class ContextVisitor extends ASTVisitor {
 	private int cursorPosition;
+	private boolean topClassVisited = false;
+	private boolean outsideTopClass = false;
 	private boolean insideMethod = false;
-	private boolean insideClass = false;
+	private boolean insideIf = false;
+	private boolean insideTry = false;
+	private boolean insideCatch = false;
+	private boolean insideFor = false;
+	private boolean isAbstract = false;
+	private boolean isFinal = false;
+	private boolean isStatic = false;
+	private boolean isInterface = false;
+	private boolean isNative = false;
+	private boolean isTransient = false;
+	private boolean isSynchronized = false;
+	private boolean isVolatile = false;
+	private String visibility = "public";
 
 	public ContextVisitor(int cursorPosition) {
 		this.cursorPosition = cursorPosition;
 	}
 
 	@Override
+	public boolean visit(TypeDeclaration node) {
+		int nodePosition = node.getStartPosition() - 1;
+		if (nodePosition >= cursorPosition && !topClassVisited) {
+			outsideTopClass = true;
+			topClassVisited = true;
+		} else {
+			if (nodePosition < cursorPosition
+					&& cursorPosition < nodePosition + node.getLength())
+				treatModifiers(node.getModifiers());
+		}
+		return true;
+	}
+
+	@Override
 	public boolean visit(MethodDeclaration node) {
 		int nodePosition = node.getStartPosition() - 1;
 		if (nodePosition < cursorPosition
-				&& cursorPosition < nodePosition + node.getLength())
+				&& cursorPosition < nodePosition + node.getLength()) {
 			insideMethod = true;
+			treatModifiers(node.getModifiers());
+		}
 		return false;
 	}
 
 	@Override
-	public boolean visit(CompilationUnit node) {
+	public boolean visit(IfStatement node) {
 		int nodePosition = node.getStartPosition() - 1;
 		if (nodePosition < cursorPosition
 				&& cursorPosition < nodePosition + node.getLength())
-			insideClass = true;
+			insideIf = true;
 		return true;
 	}
 
-	public boolean isInsideMethod() {
-		return insideMethod;
+	@Override
+	public boolean visit(TryStatement node) {
+		int nodePosition = node.getStartPosition() - 1;
+		if (nodePosition < cursorPosition
+				&& cursorPosition < nodePosition + node.getLength()) {
+			insideTry = true;
+		}
+		return true;
 	}
 
-	public boolean isInsideClass() {
-		return insideClass;
+	@Override
+	public boolean visit(CatchClause node) {
+		int nodePosition = node.getStartPosition() - 1;
+		if (nodePosition < cursorPosition
+				&& cursorPosition < nodePosition + node.getLength()) {
+			insideCatch = true;
+		}
+		return true;
+	}
+
+	@Override
+	public boolean visit(ForStatement node) {
+		int nodePosition = node.getStartPosition() - 1;
+		if (nodePosition < cursorPosition
+				&& cursorPosition < nodePosition + node.getLength()) {
+			insideFor = true;
+		}
+		return true;
+	}
+
+	private void treatModifiers(int modifiers) {
+		if (Modifier.isAbstract(modifiers))
+			isAbstract = true;
+		if (Modifier.isFinal(modifiers))
+			isFinal = true;
+		if (Modifier.isStatic(modifiers))
+			isStatic = true;
+		if (Modifier.isInterface(modifiers))
+			isInterface = true;
+		if (Modifier.isNative(modifiers))
+			isNative = true;
+		if (Modifier.isTransient(modifiers))
+			isTransient = true;
+		if (Modifier.isSynchronized(modifiers))
+			isSynchronized = true;
+		if (Modifier.isVolatile(modifiers))
+			isVolatile = true;
+		if (Modifier.isPublic(modifiers))
+			visibility = "public";
+		if (Modifier.isProtected(modifiers))
+			visibility = "protected";
+		if (Modifier.isPrivate(modifiers))
+			visibility = "private";
+	}
+
+	public CursorContext buildCursorContext(String openedFileExtension,
+			String snippetLanguage) {
+		return new CursorContext(openedFileExtension, snippetLanguage,
+				outsideTopClass, insideMethod, insideIf, insideTry,
+				insideCatch, insideFor, isAbstract, isFinal, isStatic,
+				isInterface, isNative, isTransient, isSynchronized, isVolatile,
+				visibility);
 	}
 }
