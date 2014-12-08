3ea7739a0347193f8a7b3b9ef6782ee24d3d377c
farms <farms@iscte-iul.pt>
2014-12-08 10:40:10 +0000

    [FM] - Adicionado codigo perdido devido a conflitos. Faltava este ficheiro.

--- pt.iscte.commands/src/pa/iscde/commands/features/actions/NewCommandDialog.java
+++ pt.iscte.commands/src/pa/iscde/commands/features/actions/NewCommandDialog.java
@@ -2,7 +2,7 @@ package pa.iscde.commands.features.actions;
 
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Cursor;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
@@ -10,26 +10,24 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Text;
-import pa.iscde.commands.controllers.KeyPressDetector;
+import org.eclipse.swt.widgets.TreeItem;
+
 import pa.iscde.commands.external.services.CommandViewTree;
+import pa.iscde.commands.models.CommandKey;
 import pa.iscde.commands.utils.Labels;
 
 public class NewCommandDialog extends Dialog {
 
-	private List list;
+	private CommandsList list;
 	private Button rightToLeft;
 	private Button leftToRight;
 
-	private Text inputKey;
-
 	private CommandViewTree commandsAndViews;
-	private KeyPressListener keyPressListener;
-	private Label inputKeyLabel;
 
 	protected NewCommandDialog(Shell parentShell) {
 		super(parentShell);
@@ -38,42 +36,9 @@ public class NewCommandDialog extends Dialog {
 	@Override
 	protected Control createDialogArea(Composite parent) {
 		Composite container = (Composite) super.createDialogArea(parent);
-
-		inputKeyLabel = new Label(container, SWT.NONE);
-		inputKeyLabel.setText(Labels.CLICKKEYCOMBINATIONS_LBL);
-
-		inputKey = new Text(container, SWT.BORDER);
-		inputKey.setEditable(false);
-		inputKey.setBackground(new Color(null, 255, 255, 255));
-		inputKey.setFocus();
-		inputKey.setLayoutData(new GridData(SWT.FILL, SWT.TOP, false, false));
-
-		keyPressListener = new KeyPressListener(inputKey) {
-			@Override
-			public boolean keyPressed(Event c) {
-				if(super.keyPressed(c)){
-					inputKeyLabel.setText(Labels.CLICKKEYCOMBINATIONS_LBL);
-					return true;
-				} else {
-					inputKeyLabel.setText(Labels.KEYALREADYUSE_LBL);
-					return false;
-				}
-			}
-		};
-		KeyPressDetector.getInstance().addKeyPressListener(keyPressListener);
-
 		addViewsAndCommands(container);
 
-		populateAllCommands();
-
 		return container;
-	}
-
-	private void populateAllCommands() {
-		for (int i = 0; i < 100; i++) {
-			list.add("OLAA " + i);
-			list.setData("OLAA " + i, null);
-		}
 	}
 
 	private void addViewsAndCommands(Composite container) {
@@ -91,18 +56,25 @@ public class NewCommandDialog extends Dialog {
 		gridLayout.numColumns = 3;
 		area.setLayout(gridLayout);
 
-		Composite commandsAndViewsComposite = new Composite(area, SWT.BORDER);
-		commandsAndViews = new CommandViewTree(commandsAndViewsComposite);
+		Composite commandsAndViewsComposite = new Composite(area, SWT.NONE);
+		commandsAndViewsComposite.setLayoutData(gridData);
+		commandsAndViews = new CommandViewTree(commandsAndViewsComposite, false);
 
 		Composite btns = new Composite(area, SWT.BORDER);
 		btns.setLayout(new FillLayout(SWT.VERTICAL));
 		rightToLeft = new Button(btns, SWT.NONE);
 		rightToLeft.setText("<");
+		rightToLeft.setCursor(new Cursor(null, SWT.CURSOR_HAND));
+		rightToLeft.addListener(SWT.Selection, new RightToLeftClick());
 		leftToRight = new Button(btns, SWT.NONE);
 		leftToRight.setText(">");
+		leftToRight.setCursor(new Cursor(null, SWT.CURSOR_HAND));
+		leftToRight.addListener(SWT.Selection, new LeftToRightClick());
 
-		list = new List(area, SWT.BORDER | SWT.SINGLE | SWT.V_SCROLL);
-		list.setLayoutData(gridData);
+		Composite commandsList = new Composite(area, SWT.NONE);
+		commandsList.setLayoutData(gridData);
+
+		list = new CommandsList(commandsList, 1, 2);
 	}
 
 	@Override
@@ -113,20 +85,76 @@ public class NewCommandDialog extends Dialog {
 
 	@Override
 	protected void cancelPressed() {
-		KeyPressDetector.getInstance().removeKeyPressListener(keyPressListener);
 		super.cancelPressed();
 	}
 
 	@Override
 	protected void okPressed() {
-		KeyPressDetector.getInstance().removeKeyPressListener(keyPressListener);
-		// TODO save keys in model
+		// refresh commands view
 		super.okPressed();
 	}
 
 	@Override
 	protected Point getInitialSize() {
-		return new Point(800, 500);
+		return new Point(900, 500);
+	}
+
+	private final class LeftToRightClick implements Listener {
+		@Override
+		public void handleEvent(Event event) {
+			TreeItem[] items = commandsAndViews.getCommandTree().getItems();
+			for (int i = 0; i < items.length; i++) {
+				TreeItem[] subItems = items[i].getItems();
+				for (int j = 0; j < subItems.length; j++) {
+					processSubItems(subItems[j]);
+				}
+			}
+		}
+
+		private void processSubItems(TreeItem subItem) {
+			if (subItem.getChecked()) {
+				// TODO remover da view o commando
+			}
+
+		}
+	}
+
+	private final class RightToLeftClick implements Listener {
+		@Override
+		public void handleEvent(Event event) {
+
+			TreeItem[] items = commandsAndViews.getCommandTree().getItems();
+			int oneViewSelected = 0;
+			int index = -1;
+			for (int i = 0; i < items.length; i++) {
+				if (items[i].getChecked()) {
+					oneViewSelected++;
+					index = i;
+				}
+			}
+
+			if (oneViewSelected == 1 && list.getNumSelectedItems() > 0) {
+				if (items[index].getData() instanceof String) {
+
+					CommandInputDialog dialog = new CommandInputDialog(Display
+							.getCurrent().getActiveShell(), items[index]
+							.getData().toString());
+					dialog.open();
+
+					addCommandsToView(dialog.getKey(), items[index].getData()
+							.toString()); 
+				}
+			}
+
+		}
+
+		private void addCommandsToView(CommandKey commandKey, String view) {
+			if (commandKey != null) {
+				// TODO adicionar comando a view e á tree
+
+			}
+
+		}
 	}
 
 }
