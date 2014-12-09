503a0cfefe994cbe1aab19824e280a7250a8ebda
lcmms@iscte-iul.pt <lcmms@iscte-iul.pt@1ba3a7cf-5a9b-5090-5aa7-a9fa95ec31b7>
2014-12-09 13:26:05 +0000

    Refactoring feito

--- pa.iscde.filtersearch/src/pa/iscde/filtersearch/view/ProjectBrowserSearchProvider.java
+++ pa.iscde.filtersearch/src/pa/iscde/filtersearch/view/ProjectBrowserSearchProvider.java
@@ -21,59 +21,67 @@ import pt.iscte.pidesco.projectbrowser.service.ProjectBrowserServices;
 
 public class ProjectBrowserSearchProvider implements SearchProvider {
 
-		private ProjectBrowserServices browserServices;
-		private JavaEditorServices editorServices;
-		private ViewLabelProvider labelProvider;
+	private ProjectBrowserServices browserServices;
+	private JavaEditorServices editorServices;
+	private ViewLabelProvider labelProvider;
 
-		public ProjectBrowserSearchProvider() {
+	public ProjectBrowserSearchProvider() {
 
-			Bundle bundle = FrameworkUtil.getBundle(ProjectBrowserSearchProvider.class);
-			BundleContext context  = bundle.getBundleContext();
-			ServiceReference<ProjectBrowserServices> ref = context.getServiceReference(ProjectBrowserServices.class);
-			browserServices = context.getService(ref);
+		Bundle bundle = FrameworkUtil.getBundle(ProjectBrowserSearchProvider.class);
+		BundleContext context  = bundle.getBundleContext();
+		ServiceReference<ProjectBrowserServices> ref = context.getServiceReference(ProjectBrowserServices.class);
+		browserServices = context.getService(ref);
 
-			ServiceReference<JavaEditorServices> serviceReference_javaEditor = context.getServiceReference(JavaEditorServices.class);
-			editorServices = context.getService(serviceReference_javaEditor);
-		}
-
-		@Override
-		public List<Object> getResults(String text) {
-			List<Object> hits = new ArrayList<>();
-			PackageElement root = browserServices.getRootPackage();
-
-
-
-			scan(root,hits,text);
-
-			return hits;
-		}
-
-		private void scan(PackageElement root, List<Object> hits, String text) {
-
-			for(SourceElement c : root){
-
-				if(c.getName().toUpperCase().contains(text.toUpperCase()))
-					hits.add(c);
-				if(c.isPackage()){
-					scan((PackageElement)c,hits, text);				
-				}
-			}
-		}
-
-
-		@Override
-		public Image setImage(Object object) {
-			return labelProvider.getImage(object);
-		}
-
-		@Override
-		public void doubleClickAction(TreeViewer tree, Object object) {
-
-			if (object instanceof ClassElement){
-				SourceElement element = (SourceElement) object;
-				File f = element.getFile();
-				editorServices.openFile(f);
-			}
-		}
-
+		ServiceReference<JavaEditorServices> serviceReference_javaEditor = context.getServiceReference(JavaEditorServices.class);
+		editorServices = context.getService(serviceReference_javaEditor);
 	}
+
+	@Override
+	public List<Object> getResults(String text) {
+		List<Object> hits = new ArrayList<>();
+		PackageElement root = browserServices.getRootPackage();
+		
+		scan(root,hits,text);
+		return hits;
+	}
+
+	/**
+	 * Efectua a verificação de todos os elementos incluídos no nó raiz (root). Consoante o seu tipo, será executada uma acção diferente.
+	 * No presente caso, o do ProjectBrowser, se o elemento for um package, utiliza-se recursividade do método scan; caso não seja, isto é,
+	 * se for uma classe, interface ou outro ficheiro ".java", e o seu nome contiver o texto inserido no campo de procura,
+	 * é adicionado à lista dos resultados alvo (hits).
+	 * 
+	 * @param root
+	 * @param hits
+	 * @param text
+	 */
+	private void scan(PackageElement root, List<Object> hits, String text) {
+		for(SourceElement c : root){
+			if(c.getName().toUpperCase().contains(text.toUpperCase()))
+				hits.add(c);
+			if(c.isPackage()){
+				scan((PackageElement)c,hits, text);				
+			}
+		}
+	}
+
+
+	/**
+	 * Define o icon a apresentar consoante o tipo de object passado como argumento.
+	 * 
+	 *  @param object
+	 */
+	@Override
+	public Image setImage(Object object) {
+		return labelProvider.getImage(object);
+	}
+
+	@Override
+	public void doubleClickAction(TreeViewer tree, Object object) {
+		if (object instanceof ClassElement){
+			SourceElement element = (SourceElement) object;
+			File f = element.getFile();
+			editorServices.openFile(f);
+		}
+	}
+}
