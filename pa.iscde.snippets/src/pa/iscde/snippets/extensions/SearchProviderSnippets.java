48506a1fdc4d7f1d272211dfe9f72903bb1af471
rnnma@iscte-iul.pt <rnnma@iscte-iul.pt@1ba3a7cf-5a9b-5090-5aa7-a9fa95ec31b7>
2014-12-09 19:19:33 +0000

    - Added the future extensions to other snippets

---
+++ pa.iscde.snippets/src/pa/iscde/snippets/extensions/SearchProviderSnippets.java
@@ -0,0 +1,30 @@
+package pa.iscde.snippets.extensions;
+
+import java.util.List;
+
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.swt.graphics.Image;
+
+import pa.iscde.filtersearch.providers.SearchProvider;
+
+public class SearchProviderSnippets implements SearchProvider {
+
+	@Override
+	public List<Object> getResults(String text) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Image setImage(Object object) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void doubleClickAction(TreeViewer tree, Object object) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
