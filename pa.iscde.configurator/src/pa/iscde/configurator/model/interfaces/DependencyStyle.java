e5c86788fe4307f2e64e57ba15eaa027cd97aa09
ljgds@iscte-iul.pt <ljgds@iscte-iul.pt@1ba3a7cf-5a9b-5090-5aa7-a9fa95ec31b7>
2014-12-10 21:31:03 +0000

    Added new extension point

---
+++ pa.iscde.configurator/src/pa/iscde/configurator/model/interfaces/DependencyStyle.java
@@ -0,0 +1,8 @@
+package pa.iscde.configurator.model.interfaces;
+
+import org.eclipse.swt.graphics.Color;
+
+public interface DependencyStyle {
+	Color getNodeColor();
+	Color getDependenciesColor();
+}
