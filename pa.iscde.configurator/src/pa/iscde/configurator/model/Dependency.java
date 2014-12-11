5fa3224f72be82ad1b7231161e28e35c27417f9d
ljgds <ljgds@iscte-iul.pt>
2014-12-11 14:05:57 +0000

    Changed DependencyStyle Interface Changed Dependency attributes

--- pa.iscde.configurator/src/pa/iscde/configurator/model/Dependency.java
+++ pa.iscde.configurator/src/pa/iscde/configurator/model/Dependency.java
@@ -4,10 +4,12 @@ public class Dependency {
 	
 	private Component mainComponent;
 	private Component dependantComponent;
+	private String extensionPointId;
 
-	public Dependency(Component mainComponent, Component dependantComponent) {
+	public Dependency(Component mainComponent, Component dependantComponent,String extensionPointId) {
 		this.mainComponent=mainComponent;
 		this.dependantComponent=dependantComponent;
+		this.extensionPointId=extensionPointId;
 	}
 	public Component getMainComponent() {
 		return mainComponent;
@@ -15,6 +17,10 @@ public class Dependency {
 	public Component getDependantComponent() {
 		return dependantComponent;
 	}
+	public String getExtensionPointId() {
+		return extensionPointId;
+	}
+	
 	
 
 }
