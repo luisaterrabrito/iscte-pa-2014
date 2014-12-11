8633c35d23a06114ef5f913607cd58e1cd91e5b7
ljgds <ljgds@iscte-iul.pt>
2014-12-11 13:27:12 +0000

    Added Java doc to PropertyProvider(Not generated)

--- pa.iscde.configurator/src/pa/iscde/configurator/model/interfaces/PropertyProvider.java
+++ pa.iscde.configurator/src/pa/iscde/configurator/model/interfaces/PropertyProvider.java
@@ -1,10 +1,25 @@
+/*
+ * Interface used to extend pa.iscde.configurator.propertyProvider extension Point
+ * @author João Diogo Medeiros & Luís Dias
+ * 
+ */
 package pa.iscde.configurator.model.interfaces;
 
 import java.util.List;
 
+
 public interface PropertyProvider {
 
+	/*
+	 * Get properties to insert on the view's information table.
+	 * @return List of properties to insert on table
+	 */
 	List<String> getProperties();
 	
+	/*
+	 * Get value associated with given property
+	 * @param Property to get the value from
+	 * @return Value of given property 
+	 */
 	String getValue(String property);
 }
