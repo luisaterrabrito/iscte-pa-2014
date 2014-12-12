dca6cd131599664ea239fdaf5e398716c6f995d5
rnnma@iscte-iul.pt <rnnma@iscte-iul.pt@1ba3a7cf-5a9b-5090-5aa7-a9fa95ec31b7>
2014-12-12 02:37:09 +0000

    - Created the load snippets and search methods in the search provider extension

--- pa.iscde.snippets/src/pa/iscde/snippets/extensions/SearchProviderSnippets.java
+++ pa.iscde.snippets/src/pa/iscde/snippets/extensions/SearchProviderSnippets.java
@@ -1,18 +1,66 @@
 package pa.iscde.snippets.extensions;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
+import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.graphics.Image;
 
 import pa.iscde.filtersearch.providers.SearchProvider;
 
 public class SearchProviderSnippets implements SearchProvider {
+	private static File snippetsRootFolder;
+	private HashMap<String, File> snippets;
+
+	public SearchProviderSnippets() {
+		loadSnippets();
+		URL fileURL;
+		try {
+			fileURL = new URL("platform:/plugin/pa.iscde.snippets/Snippets");
+			snippetsRootFolder = new File(FileLocator.resolve(fileURL)
+					.getPath());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	@Override
 	public List<Object> getResults(String text) {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Object> result = new ArrayList<Object>();
+		if (!text.equals("")) {
+			for (String name : snippets.keySet()) {
+				if (name.toLowerCase().contains(text.toLowerCase()))
+					result.add(name);
+			}
+		}
+		return result;
+		// TODO procurar se os snippets contem algo do texto ou se o texto
+		// TODO for o nome completo sem case de uma linguagem devolver os
+		// snippets da linguagem
+	}
+
+	private void loadSnippets() {
+		if (snippetsRootFolder.isDirectory()) {
+			snippets = new HashMap<String, File>();
+			File[] subFolders = snippetsRootFolder.listFiles();
+			for (int i = 0; i < subFolders.length; i++) {
+				if (subFolders[i].isDirectory()) {
+					File[] snippets = subFolders[i].listFiles();
+					for (int j = 0; j < snippets.length; j++) {
+						if (!snippets[j].isDirectory()) {
+							this.snippets.put(
+									snippets[j].getName().replace(".snp", ""),
+									snippets[j]);
+						}
+					}
+				}
+			}
+		}
 	}
 
 	@Override
