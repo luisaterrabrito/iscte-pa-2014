cc3400b5a0ad157a6ed354748a9600010d01e9a5
trvms@iscte-iul.pt <trvms@iscte-iul.pt@1ba3a7cf-5a9b-5090-5aa7-a9fa95ec31b7>
2014-12-13 19:23:15 +0000

    [TVM] - Inserção da contribuição do plugin commands utilizando um dos pontos de extensão existentes

---
+++ pa.iscde.commands.trvms/src/pa/iscde/commands/trms/CommandsSearch.java
@@ -0,0 +1,35 @@
+package pa.iscde.commands.trms;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.swt.graphics.Image;
+
+import pa.iscde.commands.models.CommandWarehouse;
+import pa.iscde.filtersearch.providers.SearchProvider;
+
+public class CommandsSearch implements SearchProvider {
+
+	public CommandsSearch() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public List<Object> getResults(String text) {
+		return new LinkedList<Object>(Arrays.asList(CommandWarehouse.getInstance().getAllCommandDefinitions().toArray()));
+	}
+
+	@Override
+	public Image setImage(Object object) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void doubleClickAction(TreeViewer tree, Object object) {
+		System.out.println("Cliquei: " + object);
+
+	}
+
+}
