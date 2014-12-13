20639ea83c6b82e9a3325393ec68c53fff24ad68
trvms@iscte-iul.pt <trvms@iscte-iul.pt@1ba3a7cf-5a9b-5090-5aa7-a9fa95ec31b7>
2014-12-13 22:25:36 +0000

    [TVM] - Alterado o projecto pa.iscde.commands e pa.iscde.commands.trvms de forma a mostrar os vários ícones de cada command tendo em conta o símbolo do contexto em que se aplicam!

--- pt.iscte.commands/src/pa/iscde/commands/controllers/ViewExtensionHandler.java
+++ pt.iscte.commands/src/pa/iscde/commands/controllers/ViewExtensionHandler.java
@@ -1,16 +1,19 @@
 package pa.iscde.commands.controllers;
 
 import java.util.Set;
+
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
+
 import pa.iscde.commands.controllers.ExtensionHandler.Handler;
+import pa.iscde.commands.models.ViewDef;
 
 
 public class ViewExtensionHandler implements Handler {
 
-	private Set<String> viewsInWarehouse;
+	private Set<ViewDef> viewsInWarehouse;
 	
-	public ViewExtensionHandler(Set<String> viewsWarehouse) {
+	public ViewExtensionHandler(Set<ViewDef> viewsWarehouse) {
 		this.viewsInWarehouse = viewsWarehouse;
 	}
 
@@ -18,7 +21,8 @@ public class ViewExtensionHandler implements Handler {
 	public void processExtension(IConfigurationElement e)
 			throws CoreException {
 		
-		if(viewsInWarehouse.add(e.getDeclaringExtension().getUniqueIdentifier()) == false)
+		if(viewsInWarehouse.add(new ViewDef(e.getDeclaringExtension().getUniqueIdentifier(), e.getAttribute("icon"), 
+				e.getDeclaringExtension().getContributor().getName()) ) == false)
 			System.err.println("There is duplicated views (regarding the <pluginId>.<view class name>) in the system,"
 					+ " since that some view didn't got recognised by the system. Please check the names, "
 								+ "and give unique idenfitifers.");
