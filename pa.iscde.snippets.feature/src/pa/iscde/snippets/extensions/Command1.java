648b129268315dafd77ab144b4f1f7d7f48fa2c4
dbipo <dbipo@iscte-iul.pt>
2014-12-13 18:09:12 +0000

    - Project refactor - Added "pa.iscde.snippets.feature" project for extensions

---
+++ pa.iscde.snippets.feature/src/pa/iscde/snippets/extensions/Command1.java
@@ -0,0 +1,17 @@
+package pa.iscde.snippets.extensions;
+
+import pa.iscde.commands.internal.services.Command;
+
+public class Command1 implements Command {
+
+	public Command1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void action() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
