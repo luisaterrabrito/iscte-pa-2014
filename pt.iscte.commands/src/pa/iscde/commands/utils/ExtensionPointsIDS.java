4189ceb5597a1f0df7247b2d0ce71360191179e6
farms <farms@iscte-iul.pt>
2014-12-14 17:40:44 +0000

    [FM] - Adionado o refresh da view commands.
    Mudança dos nomes das classes NewCommand e NewCommandDialog para
    CommandManager e CommandManagerDialog. 
    
    Estes nomes são mais apropriados para a funcionalidade em questão

--- pt.iscte.commands/src/pa/iscde/commands/utils/ExtensionPointsIDS.java
+++ pt.iscte.commands/src/pa/iscde/commands/utils/ExtensionPointsIDS.java
@@ -2,8 +2,9 @@ package pa.iscde.commands.utils;
 
 public enum ExtensionPointsIDS {
 
-	ACTION_ID("pa.iscde.commands.action"), COMMAND_ID("pa.iscde.commands.command"), VIEW_ID("pt.iscte.pidesco.view"),
-		REFRESH_EXPLORER_ID("pt.iscte.pidesco.projectbrowser.refresh");
+	ACTION_ID("pa.iscde.commands.action"), COMMAND_ID("pa.iscde.commands.command"), VIEW_ID("pt.iscte.pidesco.view"), REFRESH_EXPLORER_ID(
+			"pt.iscte.pidesco.projectbrowser.refresh"), 
+			REFRESH_COMMANDS_VEIW_ID("pa.iscde.commands.refreshCommands");
 
 	private String ID;
 
