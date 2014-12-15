c2ac3bf6a33336bf06875c8abd0049bc5c344344
pdcsa <pdcsa@iscte-iul.pt>
2014-12-15 16:42:50 +0000

    Extensibility (selected node from tree) finished

--- pa.iscde.outline/src/pa/iscde/outline/utility/OutlineServicesImplementation.java
+++ pa.iscde.outline/src/pa/iscde/outline/utility/OutlineServicesImplementation.java
@@ -1,31 +1,39 @@
 package pa.iscde.outline.utility;
 
 import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeItem;
+
+import pa.iscde.outline.view.OutlineView;
 
 public class OutlineServicesImplementation implements OutlineServices {
 
 	@Override
 	public ASTNode getSelectedNode() {
-		// TODO Auto-generated method stub
-		return null;
+		return OutlineView.getSingleton().getSelectedTreeItem();
 	}
 
 	@Override
 	public void selectNode(ASTNode node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void addListener(OutlineListener listener) {
-		// TODO Auto-generated method stub
-		
+		OutlineActivator.getInstance().addListener(listener);
 	}
 
 	@Override
 	public void removeListener(OutlineListener listener) {
-		// TODO Auto-generated method stub
-		
+		OutlineActivator.getInstance().removeListener(listener);
 	}
 
 }
