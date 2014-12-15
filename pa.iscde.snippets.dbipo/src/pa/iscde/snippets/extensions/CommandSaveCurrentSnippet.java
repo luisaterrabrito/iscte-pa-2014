20ba3d317a6be101109b320839155dec7193c974
dbipo <dbipo@iscte-iul.pt>
2014-12-15 15:16:45 +0000

    - Fixed imports

--- pa.iscde.snippets.dbipo/src/pa/iscde/snippets/extensions/CommandSaveCurrentSnippet.java
+++ pa.iscde.snippets.dbipo/src/pa/iscde/snippets/extensions/CommandSaveCurrentSnippet.java
@@ -1,6 +1,6 @@
 package pa.iscde.snippets.extensions;
 
-import pa.iscde.commands.internal.services.Command;
+import pa.iscde.commands.services.Command;
 import pa.iscde.snippets.gui.SnippetsView;
 
 public class CommandSaveCurrentSnippet implements Command{
