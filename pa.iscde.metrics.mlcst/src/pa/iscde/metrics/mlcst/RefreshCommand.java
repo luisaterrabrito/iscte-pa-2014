a53c960c2420466f3bcc93ea8e7882a6cfc9800a
mlcst <mlcst@iscte-iul.pt>
2014-12-16 12:55:14 +0000

    extension to the command plugin, CTRL+R to refresh the metrics table

---
+++ pa.iscde.metrics.mlcst/src/pa/iscde/metrics/mlcst/RefreshCommand.java
@@ -0,0 +1,25 @@
+package pa.iscde.metrics.mlcst;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceReference;
+
+import pa.iscde.commands.services.Command;
+import pa.iscde.metrics.extensibility.MetricsService;
+
+public class RefreshCommand implements Command{
+
+	private MetricsService services;
+
+	@Override
+	public void action() {
+		Bundle bundle = FrameworkUtil.getBundle(MetricsService.class);
+		BundleContext context = bundle.getBundleContext();
+		ServiceReference<MetricsService> reference = context
+				.getServiceReference(MetricsService.class);
+		services = context.getService(reference);
+		services.refresh();
+	}
+
+}
