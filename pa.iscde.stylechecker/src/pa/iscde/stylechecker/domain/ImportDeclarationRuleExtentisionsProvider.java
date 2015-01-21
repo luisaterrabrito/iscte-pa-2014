77bc61dd2f62bbed8308a1ac479a378516f70885
jdbms <jdbms@iscte-iul.pt>
2015-01-21 18:19:04 +0000

    + Trying to add external dependencies but not working yet + Refactoring system architecture

--- pa.iscde.stylechecker/src/pa/iscde/stylechecker/domain/ImportDeclarationRuleExtentisionsProvider.java
+++ pa.iscde.stylechecker/src/pa/iscde/stylechecker/domain/ImportDeclarationRuleExtentisionsProvider.java
@@ -6,11 +6,13 @@ import java.util.List;
 import org.eclipse.core.runtime.IExtension;
 
 import pa.iscde.stylechecker.internal.extension.AbstractImportDeclarationRule;
+import pa.iscde.stylechecker.internal.extension.ImportDeclarationNoWildCardRule;
 import pa.iscde.stylechecker.model.AbstractStyleRuleExensionProvider;
 
 public class ImportDeclarationRuleExtentisionsProvider extends
 		AbstractStyleRuleExensionProvider {
 	
+	private static final String DEFAULT_INTERNAL_EXTENSION_PACKAGE = "pa.iscde.stylechecker.internal.extension";
 	private static final String IMPORT_DECLARATION_EXT_POINT = "pa.iscde.stylechecker.importstyle";//TODO;
 
 	public static List<AbstractImportDeclarationRule> getExtentions() {
@@ -19,6 +21,12 @@ public class ImportDeclarationRuleExtentisionsProvider extends
 		for (IExtension extension : extensions) {
 			rules.add((AbstractImportDeclarationRule) extension);
 		}
+		return rules;
+	}
+	
+	public static List<AbstractImportDeclarationRule> getInternalExtentions() {
+		List<AbstractImportDeclarationRule> rules = new ArrayList<AbstractImportDeclarationRule>();
+		rules.add(new ImportDeclarationNoWildCardRule());
 		return rules;
 	}
 
