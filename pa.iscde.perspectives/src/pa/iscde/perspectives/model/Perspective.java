a40ff27e6b069f49d8fcd809f1ef32861f24b0b3
hmcgf@iscte.pt <hmcgf@iscte.pt@1ba3a7cf-5a9b-5090-5aa7-a9fa95ec31b7>
2015-01-23 15:51:56 +0000

    Final Changes

--- pa.iscde.perspectives/src/pa/iscde/perspectives/model/Perspective.java
+++ pa.iscde.perspectives/src/pa/iscde/perspectives/model/Perspective.java
@@ -10,6 +10,33 @@ import pt.iscte.pidesco.extensibility.ViewLocation;
 
 public class Perspective implements Serializable
 {
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Perspective other = (Perspective) obj;
+		if (id == null)
+		{
+			if (other.id != null)
+				return false;
+		}
+		else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 	private static final long	serialVersionUID	= 2376147722388607017L;
 	private transient Image		icon;
 	private String				id;
@@ -74,5 +101,10 @@ public class Perspective implements Serializable
 			toReturn.addAll(view.getViewLocations());
 		}
 		return toReturn;
+	}
+	@Override
+	public String toString()
+	{
+		return getName() + " (source:  " + getId() + ")";
 	}
 }
